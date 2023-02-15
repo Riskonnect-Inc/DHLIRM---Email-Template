163dbbabb256e60e3bf4addc1f067e9b2aa8dea6
mulvelingrk <33529770+mulvelingrk@users.noreply.github.com>
2023-02-15 14:18:09 -0500

    Add files via upload
    This component is meant to reside in a Utility Bar (with an aura wrapper of same name), with as small a footprint as possible (auto-loaded), to inject the following CSS "hacks":
    1. For Claims only: It suppresses the standard timeline list, while keeping its standard composer visible
    2. It suppresses Reply/Reply All/Fwd buttons on the EmailMessage detail page, but only when the email is related to a Claim

---
+++ force-app/main/default/lwc/utilityBarTimelineStylerInner/utilityBarTimelineStylerInner.js
@@ -0,0 +1,29 @@
+import { LightningElement, api } from 'lwc';
+import getRecordInfo from "@salesforce/apex/UtilityStyleHackController.getRecordInfo";
+
+import { loadStyle } from 'lightning/platformResourceLoader';
+import claimStylesOn from '@salesforce/resourceUrl/claimStylesOn';
+import claimStylesOff from '@salesforce/resourceUrl/claimStylesOff';
+
+export default class UtilityStyleHack extends LightningElement {
+    @api
+    get recordId() {
+        return this._recordId;
+    }
+
+    objType;
+
+    set recordId(value) {
+        //console.log('Setting Record ID: ' + value);
+        this._recordId = value;
+        getRecordInfo({ recordId: value })
+            .then((result) => {
+                console.log('Result: ' + JSON.stringify(result));
+                this.objType = result.objectType;
+                loadStyle(this, result.emailTargetType || result.objectType === 'Claim__c' ? claimStylesOn : claimStylesOff);
+            }).catch(error => {
+                console.error('Error: ' + JSON.stringify(error));
+                loadStyle(this, claimStylesOff);
+            });
+    }
+}
